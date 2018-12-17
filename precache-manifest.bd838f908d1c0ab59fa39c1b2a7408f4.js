0fc88076cbd11ca6fc4ae97a6d19722559610354
Pablo Gamito <pablo@gamito.me>
2018-12-17 22:12:15 +0100

    Updates

---
+++ precache-manifest.bd838f908d1c0ab59fa39c1b2a7408f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b8463fe108e6ea781af",
+    "url": "/static/css/main.1f8c8356.chunk.css"
+  },
+  {
+    "revision": "4b8463fe108e6ea781af",
+    "url": "/static/js/main.4b8463fe.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6a1c70ed7cd37b8b89c8f83ca4dfe56c",
+    "url": "/index.html"
+  }
+];
